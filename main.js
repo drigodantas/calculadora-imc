ccfa6c75e0d7eab3116a4bbf3e002e8562d4dc45
Rodrigo Dantas <drigowpd@gmail.com>
2022-07-10 16:39:23 -0300

    Upando calculadora de IMC

---
+++ main.js
@@ -0,0 +1,64 @@
+const inputWeight = document.querySelector("#weight")
+const inputHeight = document.querySelector("#height")
+const divIMC = document.querySelector("#result")
+
+function imcCalculate() {
+    const weight = Number(inputWeight.value)
+    const height = Number(inputHeight.value)
+    const imc = calculateIMC(weight, height)
+    const p = paragraphCreator()
+    const levelimc = levelIMC(imc)
+
+    divIMC.innerHTML = ""
+    divIMC.appendChild(p)
+
+    if (height <= 0 || height === NaN || height >= 2.5) {
+        p.innerHTML = "Altura Inválida"
+        p.classList.add('error')
+        return
+    }
+
+    if (weight <= 0 || weight == NaN || weight >= 250) {
+        p.innerHTML = "Peso Inválido"
+        p.classList.add('error')
+        return
+    }
+    
+    p.classList.add('sucess')
+    p.innerHTML = `O seu IMC é <strong>${imc}</strong> está em <strong>${levelimc}</strong>`
+
+}
+
+function calculateIMC (weight, height) {
+    const imc = weight / (height ** 2)
+    return imc.toFixed(1)
+
+}
+
+function levelIMC(imc) {
+    if (imc < 18.5) {
+        return "Peso baixo"
+    }
+    if (imc < 24.9) {
+        return "Peso normal"
+    }
+    if (imc < 29.9) {
+        return "Sobrepeso"
+    }
+    if (imc < 34.9) {
+        return "Obesidade Grau I"
+    }
+    if (imc < 39.9) {
+        return "Obesidade Grau II"
+    }
+    if (imc >= 39.9) {
+        return "Obesidade Grau III"
+    }
+
+}
+
+function paragraphCreator() {
+    const p = document.createElement('p')
+    return p
+}
+
